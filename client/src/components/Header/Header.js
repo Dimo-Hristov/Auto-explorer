496388067ea0ae248cf88bf100013b01dbe584fb
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-21 13:16:05 +0200

    add conditional rendering in navigation

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,18 +5,27 @@ import { useContext } from 'react';
 
 export const Header = () => {
 
-    const { email } = useContext(AuthContext)
+    const { email } = useContext(AuthContext);
     return (
         <header className={headerSyles.header}>
             <nav>
                 <ul>
                     <li><Link to={'/'}>Home</Link></li>
                     <li><Link to={'/catalog'}>Catalog</Link></li>
-                    <li><Link to={'/publish'}>Publish car</Link></li>
-                    <li><Link to={'/profile'}>Profile</Link></li>
-                    <li><Link to={'/login'}>Login</Link></li>
-                    <li><Link to={'/register'}>Register</Link></li>
-                    <li><Link to={'/logout'}>Logout</Link></li>
+
+                    {email ? (
+                        <>
+                            <li><Link to={'/publish'}>Publish car</Link></li>
+                            <li><Link to={'/profile'}>Profile</Link></li>
+                            <li><Link to={'/logout'}>Logout</Link></li>
+                        </>
+                    ) : (
+                        <>
+                            <li><Link to={'/login'}>Login</Link></li>
+                            <li><Link to={'/register'}>Register</Link></li>
+                        </>
+                    )}
+
 
                 </ul>
                 <span>Hello {email}</span>
