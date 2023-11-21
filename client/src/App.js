d8c07d9a59341ec2e8387b5b7c2b1c0e8a891cc1
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-21 10:13:39 +0200

    create authProcider component and move the logic there

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,21 @@
 import './global.css';
 import { Header } from "./components/Header/Header";
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './components/HomePage/HomePage';
 import { CatalogPage } from './components/CatalogPage/CatalogPage';
 import { PublishPage } from './components/PublishPage/PublishPage';
 import { ProfilePage } from './components/ProfilePage/ProfilePage';
 import { LoginPage } from './components/LoginPage/LoginPage';
 import { RegisterPage } from './components/RegisterPage/RegisterPage';
-import { AuthContext } from './contexts/AuthContext';
-import * as authService from './services/authService';
-import { passwordValidator } from './validators/passwordValidator';
-import { useState } from 'react';
+import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
-    const navigate = useNavigate()
-    const [auth, setAuth] = useState({});
 
-    const onRegisterSubmit = async (formValues) => {
-        const { rePassword, ...data } = formValues;
-
-        const passwordMatch = passwordValidator(data.password, rePassword);
-
-        if (!passwordMatch) {
-            alert('Password missmatch!');
-            return;
-        }
-
-        try {
-            const user = await authService.register(data);
-            setAuth(user);
-            navigate('/');
-        } catch (error) {
-            setAuth({})
-            alert(error.message)
-        }
-    }
-
-    const context = {
-        onRegisterSubmit,
-        accessToken: auth.accessToken,
-        username: auth.username,
-        userId: auth._id,
-    }
 
     return (
         <>
-            <AuthContext.Provider value={context}>
+            <AuthProvider>
                 <Header />
                 <main className="app">
                     <Routes>
@@ -57,7 +27,7 @@ function App() {
                         <Route path='/register' element={<RegisterPage />} />
                     </Routes>
                 </main>
-            </AuthContext.Provider>
+            </AuthProvider>
         </>
     );
 }
--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,3 +1,50 @@
 import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { passwordValidator } from "../validators/passwordValidator";
+import * as authService from '../services/authService';
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children
+}) => {
+    const navigate = useNavigate()
+
+    const onRegisterSubmit = async (formValues) => {
+        const { rePassword, ...data } = formValues;
+
+        const passwordMatch = passwordValidator(data.password, rePassword);
+
+        if (!passwordMatch) {
+            alert('Password missmatch!');
+            return;
+        }
+
+        try {
+            const user = await authService.register(data);
+            setAuth(user);
+            navigate('/');
+        } catch (error) {
+            setAuth({})
+            alert(error.message)
+        }
+    }
+
+    const [auth, setAuth] = useState({});
+
+    const contextValues = {
+        onRegisterSubmit,
+        accessToken: auth.accessToken,
+        username: auth.username,
+        userId: auth._id,
+    }
+
+    return (
+        <>
+            <AuthContext.Provider value={contextValues}>
+                {children}
+            </AuthContext.Provider>
+        </>
+    )
+}
