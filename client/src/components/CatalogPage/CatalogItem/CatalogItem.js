260284b2f099499a0dd33528250b374e25d6c14c
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-22 18:12:46 +0200

    add navigation on list item

--- client/src/components/CatalogPage/CatalogItem/CatalogItem.js
+++ client/src/components/CatalogPage/CatalogItem/CatalogItem.js
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom'
 import catalogItemStyles from './catalogItem.module.css'
 
 export const CatalogItem = ({
     car
 }) => {
     return (
-        <li className={catalogItemStyles.catalogItem}>
-            <img src={car.imageUrl} alt={car.brand} />
-            <h2>{car.brand} - {car.model}</h2>
-            <p>Engine: {car.engine}</p>
+        <li className={catalogItemStyles.catalogItem} navi>
+            <Link to={`/catalog/${car._id}`}>
+                <img src={car.imageUrl} alt={car.brand} />
+                <h2>{car.brand} - {car.model}</h2>
+                <p>Engine: {car.engine}</p>
+            </Link>
         </li>
     )
 }
