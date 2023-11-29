3b749d59d72b13d40a3f12d9632aa16791b0c5e3
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-29 10:32:59 +0200

    set profile page functionality
    add 2 buttons, Show likerd cars and show uploaded cars and render them

--- client/src/components/ProfilePage/ProfilePage.js
+++ client/src/components/ProfilePage/ProfilePage.js
@@ -1,25 +1,49 @@
-import { Navigation } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
+import { useContext, useState } from 'react';
+import { CarContext } from '../../contexts/CarContext';
+
 import profilePageStyles from './profilePage.module.css';
+import { AuthContext } from '../../contexts/AuthContext';
+import { CatalogItem } from '../CatalogPage/CatalogItem/CatalogItem';
 
 export const ProfilePage = () => {
+    const { cars } = useContext(CarContext);
+    const { userId } = useContext(AuthContext)
+
+    const [filteredCars, setFilteredCars] = useState([])
+
+    const showLikedCars = () => {
+        const likedCars = cars.filter((car) =>
+            car.likes.some((like) => like._ownerId === userId)
+        );
+
+        setFilteredCars(likedCars);
+    }
+
+    const showUploadedCars = () => {
+        const uploadedCars = cars.filter((car) => car._ownerId === userId);
+        setFilteredCars(uploadedCars)
+    }
+
+
     return (
         <section className={profilePageStyles.profile}>
+            <div className="buttons">
+                <button onClick={showLikedCars}>Show liked cars</button>
+                <button onClick={showUploadedCars}>Show uploaded cars</button>
+            </div>
 
 
-            <swiper-container
-                slides-per-view={3}
-                breakpoints={{
-                    768: {
-                        slidesPerView: 4,
-                    },
-                }}
-            >
-                ...
-            </swiper-container>
+            {filteredCars.lerngth < 1 ? (
+                <h3>Not records found</h3>
+            ) : (
+                <ul>
+                    {filteredCars.map(x => <CatalogItem key={x._id} car={x} />)}
+                </ul>
+            )
+
+            }
+
 
         </section>
-    )
-}
+    );
+};
