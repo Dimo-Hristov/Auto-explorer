e997dd3a2e1ab2646bd6bdfb8320e82e1afc6b88
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-12-02 22:37:23 +0200

    add form validation for login

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
 import { useForm } from "../../hooks/useForm"
 import { AuthContext } from "../../contexts/AuthContext";
 import { useContext } from "react";
+import { useFormValidate } from "../../hooks/useFormValidate";
 
 export const LoginPage = () => {
     const { onLoginSubmit } = useContext(AuthContext);
@@ -10,30 +11,46 @@ export const LoginPage = () => {
         password: '',
     }, onLoginSubmit);
 
+    const { validatorsHandler, errors } = useFormValidate(formValues)
+
     return (
         <section>
             <h1>login</h1>
 
             <form onSubmit={onSubmit}>
                 <label htmlFor="email">e-mail</label>
+                {errors.email && (
+                    <p>{errors.email}</p>
+                )}
                 <input type="text"
                     name="email"
                     id="email"
                     placeholder="John@gmail.com"
-                    value={formValues.username}
+                    value={formValues.email}
                     onChange={onChange}
-                    autoComplete="current-username" />
+                    autoComplete="current-username"
+                    onBlur={validatorsHandler('email')}
+                />
 
                 <label htmlFor="password">Password</label>
+                {errors.password && (
+                    <p>{errors.password}</p>
+                )}
                 <input type="password"
                     name="password"
                     id="password"
                     value={formValues.password}
                     onChange={onChange}
                     autoComplete="current-password"
+                    onBlur={validatorsHandler('password')}
                 />
 
-                <input type="submit" value='Submit' className='submitButton' />
+                <input
+                    type="submit"
+                    value="Submit"
+                    className={`submitButton ${Object.values(errors).some(error => error !== '') ? 'disabled' : 'active'}`}
+                    disabled={Object.values(errors).some(error => error !== '')}
+                />
             </form>
         </section>
     )
