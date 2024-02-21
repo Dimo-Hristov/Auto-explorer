aef96b2dd39702e1f72a053a7ea3b844e6055346
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-02-21 14:41:05 +0200

    add hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,24 @@
 import headerSyles from './header.module.css';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ErrorContext } from '../../contexts/ErrorContext';
 
 export const Header = () => {
 
     const { email } = useContext(AuthContext);
-
     const { errorMessages } = useContext(ErrorContext);
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
     return (
         <>
             <header className={headerSyles.header}>
-                <nav>
-                    <ul>
+                <nav onClick={toggleMenu}>
+                    <ul className={` ${isMenuOpen ? headerSyles.open : ''}`} >
                         <li><Link to={'/home'}>Home</Link></li>
                         <li><Link to={'/catalog'}>Catalog</Link></li>
 
@@ -36,17 +41,24 @@ export const Header = () => {
                         <p className='user-email'>Hello {email}</p>
                     )}
 
+                    <div className={`${headerSyles.hamburger} ${isMenuOpen ? headerSyles.open : ''}`} onClick={toggleMenu}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+
                 </nav>
 
 
 
-            </header>
+            </header >
 
             {errorMessages && (
                 <div className={headerSyles.errors}>
                     {errorMessages.map((message, index) => <p className={headerSyles.errorsP} key={`${index}${message}`}>{message}</p>)}
                 </div>
-            )}
+            )
+            }
         </>
     )
 }
